06e5409d7c60159afe577bf347e2fde0ad72e7bb
ahmer-mt <ahmer.ali@mindtitan.com>
2025-08-06 21:24:39 +0500

    fix rollback

--- DSL/Liquibase/changelog/20250624225925-rollback.sql
+++ DSL/Liquibase/changelog/20250624225925-rollback.sql
@@ -1,11 +1,15 @@
 -- liquibase formatted sql
 -- changeset ahmer-mt:20250624225925 ignore:true
-DROP TABLE IF EXISTS logs;
-DROP TABLE IF EXISTS scrape_sources_task;
-DROP TABLE IF EXISTS source_run;
-DROP TABLE IF EXISTS source;
-DROP TABLE IF EXISTS domain;
-DROP TABLE IF EXISTS client;
 
-DROP TYPE IF EXISTS status_type;
+DROP TABLE IF EXISTS source_file CASCADE;
+DROP TABLE IF EXISTS source_run_page CASCADE;
+DROP TABLE IF EXISTS source_run_report CASCADE;
+DROP TABLE IF EXISTS source CASCADE;
+DROP TABLE IF EXISTS agency CASCADE;
+
+-- Drop custom ENUM types
+DROP TYPE IF EXISTS source_file_type;
+DROP TYPE IF EXISTS source_file_status_type;
+DROP TYPE IF EXISTS source_status_type;
 DROP TYPE IF EXISTS source_type;
+DROP TYPE IF EXISTS agency_type;
