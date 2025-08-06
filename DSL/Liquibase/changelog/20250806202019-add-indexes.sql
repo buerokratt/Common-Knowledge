81be6b5a021c01021785d0b617f83683aef9a808
ahmer-mt <ahmer.ali@mindtitan.com>
2025-08-06 20:43:07 +0500

    https://github.com/buerokratt/Common-Knowledge/issues/56 SCHEMA-004

---
+++ DSL/Liquibase/changelog/20250806202019-add-indexes.sql
@@ -0,0 +1,2 @@
+-- liquibase formatted sql
+-- changeset ahmer-mt:20250806202019 ignore:true
--- DSL/Resql/ckb/GET/agency/get_dirty_agency.sql
+++ DSL/Resql/ckb/GET/agency/get_dirty_agency.sql
@@ -2,12 +2,11 @@ SELECT COALESCE(
     (WITH latest_records AS (
         SELECT DISTINCT ON (base_id) base_id, zip_dirty, is_zipping
         FROM agency
-        WHERE is_deleted = FALSE
         ORDER BY base_id, updated_at DESC
     )
     SELECT base_id::text
     FROM latest_records
-    WHERE zip_dirty = TRUE AND is_zipping = FALSE
+    WHERE is_deleted = FALSE AND zip_dirty = TRUE AND is_zipping = FALSE
     LIMIT 1), 
     ''
 ) as base_id;
