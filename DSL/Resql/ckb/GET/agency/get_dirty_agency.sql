a24b07f8e43ca96c8d2cf80ddc0aca3a32e67ecd
ahmer-mt <ahmer.ali@mindtitan.com>
2025-08-08 06:36:52 +0500

    optimize scheduling

--- DSL/Resql/ckb/GET/agency/get_dirty_agency.sql
+++ DSL/Resql/ckb/GET/agency/get_dirty_agency.sql
@@ -16,15 +16,13 @@ declaration:
         type: string
         description: "base id of agency"
 */
-SELECT COALESCE(
-    (WITH latest_records AS (
-        SELECT DISTINCT ON (base_id) base_id, zip_dirty, is_zipping, is_deleted
-        FROM agency
-        ORDER BY base_id, updated_at DESC
-    )
-    SELECT base_id::text
-    FROM latest_records
-    WHERE is_deleted = FALSE AND zip_dirty = TRUE AND is_zipping = FALSE
-    LIMIT 1), 
-    ''
-) as base_id;
+
+WITH latest_records AS (
+    SELECT DISTINCT ON (base_id) base_id, zip_dirty, is_zipping, is_deleted
+    FROM agency
+    ORDER BY base_id, updated_at DESC
+)
+SELECT base_id::text
+FROM latest_records
+WHERE is_deleted = FALSE AND zip_dirty = TRUE AND is_zipping = FALSE
+LIMIT 1;
