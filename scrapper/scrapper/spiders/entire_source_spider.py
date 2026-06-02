79d2991942c761af66998ce50e67a4d7881b3e77
ruwinirathnamalala <ruwini.rathnamalala@rootcodelabs.com>
2026-06-02 19:02:15 +0530

    ruff-lint issue fixed

--- scrapper/scrapper/spiders/entire_source_spider.py
+++ scrapper/scrapper/spiders/entire_source_spider.py
@@ -1,5 +1,6 @@
 import datetime
 from collections.abc import Iterator
+from typing import Any
 
 import requests
 
@@ -22,7 +23,7 @@ class EntireSourceSpider(SpecifiedPagesSpider):
             self.urls = []
 
     @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs) -> "EntireSourceSpider":
+    def from_crawler(cls, crawler: Any, *args: object, **kwargs: object) -> "EntireSourceSpider":
         """Create spider and perform crawler-dependent initialization.
 
         Scrapy calls the classmethod `from_crawler` which attaches the
